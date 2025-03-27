bc3a9c03b144cbbaae209bb1a819157121b3c95f
hoangbao2709 <hoangbao27092004@gmail.com>
2025-03-27 16:02:04 +0700

    Bao

---
+++ Node_Simple/web/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import './App.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Admin } from './app/pages/Admin/index';
+import { Home } from './app/pages/Admin/home';
+import axios from 'axios';
+
+export function App() {
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />  
+          <Route path="admin/*" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ Node_Simple/web/src/app/pages/review/script.js
@@ -0,0 +1,13 @@
+<script>
+    var swiper = new Swiper(".default-carousel", {
+    loop: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+    });
+</script>
